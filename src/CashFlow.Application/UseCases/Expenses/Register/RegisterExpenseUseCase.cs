ee912c6729fa5ff989f84cb020891bc6cb2a3505
Welisson Arley <welissonarleyvs@gmail.com>
2024-03-26 20:17:16 +0000

    feat: adding validation to request save expense

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -6,8 +7,34 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
-        //TO DO VALIDATIONS
+        Validate(request);
 
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if(titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required.");
+        }
+
+        if(request.Amount <= 0)
+        {
+            throw new ArgumentException("The Amount must be greater than zero.");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if(result > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if(paymentTypeIsValid == false)
+        {
+            throw new ArgumentException("Payment Type is not valid.");
+        }
     }
 }
