99a4a23a6011c7fd7b568fc6e721322e91f5fdc2
Welisson Arley <welissonarleyvs@gmail.com>
2024-03-28 17:25:41 +0000

    feat: return a list of error in the response error json

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,6 +1,7 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -20,15 +21,15 @@ public class ExpensesController : ControllerBase
 
             return Created(string.Empty, response);
         }
-        catch (ArgumentException ex)
+        catch (ErrorOnValidationException ex)
         {
-            var errorResponse = new ResponseErrorJson(ex.Message);
+            var errorResponse = new ResponseErrorJson(ex.Errors);
 
             return BadRequest(errorResponse);
         }
         catch
         {
-            var errorResponse = new ResponseErrorJson("unknown error");
+            var errorResponse = new ResponseErrorJson("Unknown error");
 
             return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
         }
--- src/CashFlow.Communication/Responses/ResponseErrorJson.cs
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -1,10 +1,15 @@
 ﻿namespace CashFlow.Communication.Responses;
 public class ResponseErrorJson
 {
-    public string ErrorMessage { get; set; } = string.Empty;
+    public List<string> ErrorMessages { get; set; }
 
     public ResponseErrorJson(string errorMessage)
     {
-        ErrorMessage = errorMessage;
+        ErrorMessages = [errorMessage];
+    }
+
+    public ResponseErrorJson(List<string> errorMessage)
+    {
+        ErrorMessages = errorMessage;
     }
 }
