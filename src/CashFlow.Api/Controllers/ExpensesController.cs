184fb10221ed9cce468169974b06860bfa4e0551
Welisson Arley <welissonarleyvs@gmail.com>
2024-03-28 12:08:29 +0000

    feat: improving error responses

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -21,11 +22,15 @@ public class ExpensesController : ControllerBase
         }
         catch (ArgumentException ex)
         {
-            return BadRequest(ex.Message);
+            var errorResponse = new ResponseErrorJson(ex.Message);
+
+            return BadRequest(errorResponse);
         }
         catch
         {
-            return StatusCode(StatusCodes.Status500InternalServerError, "unknown error");
+            var errorResponse = new ResponseErrorJson("unknown error");
+
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
         }
     }
 }
---
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,10 @@
+﻿namespace CashFlow.Communication.Responses;
+public class ResponseErrorJson
+{
+    public string ErrorMessage { get; set; } = string.Empty;
+
+    public ResponseErrorJson(string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+    }
+}
