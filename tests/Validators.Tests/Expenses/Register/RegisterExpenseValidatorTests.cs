2fb1fa45373855b475e0dee85fe75ddd988fb9e8
Welisson Arley <welissonarleyvs@gmail.com>
2024-03-30 08:17:19 +0000

    tests: adding our first success test

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "Description",
+            Title = "Apple",
+            PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
+        };
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+}
