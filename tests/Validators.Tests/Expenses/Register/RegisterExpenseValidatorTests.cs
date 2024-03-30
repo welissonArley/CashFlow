12c8d402d0d7817f23c0ec2a78b92ff316f06c55
Welisson Arley <welissonarleyvs@gmail.com>
2024-03-30 09:24:55 +0000

    tests: adding error test for expense title

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 
@@ -17,5 +18,21 @@ public class RegisterExpenseValidatorTests
 
         //Assert
         result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
     }
 }
