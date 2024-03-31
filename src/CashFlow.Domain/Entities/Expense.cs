743e912e060513cce05915042cf719ea23fcda35
Welisson Arley <welissonarleyvs@gmail.com>
2024-03-31 10:50:49 +0100

    feat: adding expense entity in domain

---
+++ src/CashFlow.Domain/Entities/Expense.cs
@@ -0,0 +1,12 @@
+﻿using CashFlow.Domain.Enums;
+
+namespace CashFlow.Domain.Entities;
+public class Expense
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime Date { get; set; }
+    public decimal Amount { get; set; }
+    public PaymentType PaymentType { get; set; }
+}
---
+++ src/CashFlow.Domain/Enums/PaymentType.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.Domain.Enums;
+public enum PaymentType
+{
+    Cash = 0,
+    CreditCard = 1,
+    DebitCard = 2,
+    EletronicTransfer = 3,
+}
