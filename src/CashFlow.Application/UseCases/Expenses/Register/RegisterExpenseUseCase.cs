ccd5f901ed5ff87137dbe09f75b6b2bd47c883c4
Welisson Arley <welissonarleyvs@gmail.com>
2024-04-17 20:06:13 +0100

    feat: adding and configuring dbcontext

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Domain.Entities;
 using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -8,6 +9,15 @@ public class RegisterExpenseUseCase
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
         Validate(request);
+
+        var entity = new Expense
+        {
+            Amount = request.Amount,
+            Date = request.Date,
+            Description = request.Description,
+            Title = request.Title,
+            PaymentType = (Domain.Enums.PaymentType)request.PaymentType,
+        };
 
         return new ResponseRegisteredExpenseJson();
     }
---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=@Password123;";
+
+        var version = new Version(8, 0, 35);
+        var serverVersion = new MySqlServerVersion(version);
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
