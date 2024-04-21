a6032e61027c05841040fa7520d6cfb9a2725595
Welisson Arley <welissonarleyvs@gmail.com>
2024-04-21 09:27:14 +0100

    feat: using AsNoTracking

--- src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -18,6 +18,6 @@ internal class ExpensesRepository : IExpensesRepository
 
     public async Task<List<Expense>> GetAll()
     {
-        return await _dbContext.Expenses.ToListAsync();
+        return await _dbContext.Expenses.AsNoTracking().ToListAsync();
     }
 }
