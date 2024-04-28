631b1842d5deff1a08b940f8b59a86f71da58632
Welisson Arley <welissonarleyvs@gmail.com>
2024-04-28 15:09:51 +0100

    feat: adding endpoit to generate excel file

---
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReportController : ControllerBase
+{
+    [HttpGet("excel")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetExcel()
+    {
+        byte[] file = new byte[1];
+
+        if(file.Length > 0 )
+            return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+
+        return NoContent();
+    }
+}
