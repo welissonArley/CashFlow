db042e37127d0fdb66d462c4d9744907e054f2a2
Welisson Arley <welissonarleyvs@gmail.com>
2024-04-29 15:52:19 +0100

    feat: adding keyword using

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -22,7 +22,7 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
             return [];
         }
 
-        var workbook = new XLWorkbook();
+        using var workbook = new XLWorkbook();
 
         workbook.Author = "Welisson Arley";
         workbook.Style.Font.FontSize = 12;
