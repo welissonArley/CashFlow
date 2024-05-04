11f246fc674a01703a85eadeb0f4be5b9b924cfe
Welisson Arley <welissonarleyvs@gmail.com>
2024-05-04 11:12:35 +0100

    feat: adding document

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,5 +1,7 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
+using MigraDoc.DocumentObjectModel;
 using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
@@ -23,8 +25,21 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
             return [];
         }
 
-        
+        var document = CreateDocument(month);
 
         return [];
+    }
+
+    private Document CreateDocument(DateOnly month)
+    {
+        var document = new Document();
+
+        document.Info.Title = $"{ResourceReportGenerationMessages.EXPENSES_FOR} {month:Y}";
+        document.Info.Author = "Welisson Arley";
+
+        var style = document.Styles["Normal"];
+        style!.Font.Name = FontHelper.RALEWAY_REGULAR;
+
+        return document;
     }
 }
--- src/CashFlow.Domain/Reports/ResourceReportGenerationMessages.Designer.cs
+++ src/CashFlow.Domain/Reports/ResourceReportGenerationMessages.Designer.cs
@@ -124,6 +124,15 @@ namespace CashFlow.Domain.Reports {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Expenses for.
+        /// </summary>
+        public static string EXPENSES_FOR {
+            get {
+                return ResourceManager.GetString("EXPENSES_FOR", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Payment Type.
         /// </summary>
         public static string PAYMENT_TYPE {
