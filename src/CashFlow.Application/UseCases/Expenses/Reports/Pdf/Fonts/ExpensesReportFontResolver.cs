0ded64f0bda12937a382743c116d80fcaaa9b4f7
Welisson Arley <welissonarleyvs@gmail.com>
2024-05-04 10:55:41 +0100

    feat: get bytes for fonts file

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -1,15 +1,33 @@
 ﻿using PdfSharp.Fonts;
+using System.Reflection;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 public class ExpensesReportFontResolver : IFontResolver
 {
     public byte[]? GetFont(string faceName)
     {
-        throw new NotImplementedException();
+        var stream = ReadFontFile(faceName);
+
+        stream ??= ReadFontFile(FontHelper.DEFAULT_FONT);
+
+        var length = (int)stream!.Length;
+
+        var data = new byte[length];
+
+        stream.Read(buffer: data, offset: 0, count: length);
+
+        return data;
     }
 
     public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
     {
         return new FontResolverInfo(familyName);
+    }
+
+    private Stream? ReadFontFile(string faceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        return assembly.GetManifestResourceStream($"CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts.{faceName}.ttf");
     }
 }
--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/FontHelper.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/FontHelper.cs
@@ -5,4 +5,6 @@ public class FontHelper
     public const string RALEWAY_BLACK = "Raleway-Black";
     public const string WORKSANS_REGULAR = "WorkSans-Regular";
     public const string WORKSANS_BLACK = "WorkSans-Black";
+
+    public const string DEFAULT_FONT = RALEWAY_REGULAR;
 }
