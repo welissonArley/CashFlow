39b035894db375515a5f4128de27e7ea6a476dd7
Welisson Arley <welissonarleyvs@gmail.com>
2024-05-05 10:26:54 +0100

    feat: adding first line for each expense table

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -40,6 +40,25 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
         foreach (var expense in expenses)
         {
             var table = CreateExpenseTable(page);
+
+            var row = table.AddRow();
+            row.Height = 25;
+
+            row.Cells[0].AddParagraph(expense.Title);
+            row.Cells[0].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 14, Color = ColorsHelper.BLACK };
+            row.Cells[0].Shading.Color = ColorsHelper.RED_LIGHT;
+            row.Cells[0].VerticalAlignment = VerticalAlignment.Center;
+            row.Cells[0].MergeRight = 2;
+            row.Cells[0].Format.LeftIndent = 20;
+
+            row.Cells[3].AddParagraph(ResourceReportGenerationMessages.AMOUNT);
+            row.Cells[3].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 14, Color = ColorsHelper.WHITE };
+            row.Cells[3].Shading.Color = ColorsHelper.RED_DARK;
+            row.Cells[3].VerticalAlignment = VerticalAlignment.Center;
+
+            row = table.AddRow();
+            row.Height = 30;
+            row.Borders.Visible = false;
         }
 
         return RenderDocument(document);
