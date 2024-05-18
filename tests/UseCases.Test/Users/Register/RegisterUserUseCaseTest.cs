82a7ec01da041d99c8a10a2fc029153566ebde7f
Welisson Arley <welissonarleyvs@gmail.com>
2024-05-18 18:33:17 +0100

    tests: starting usecases test

---
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace UseCases.Test.Users.Register;
+public class RegisterUserUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var useCase = CreateUseCase();
+
+        var result = await useCase.Execute(request);
+
+        result.Should().NotBeNull();
+        result.Name.Should().Be(request.Name);
+        result.Token.Should().NotBeNullOrWhiteSpace();
+    }
+
+    private RegisterUserUseCase CreateUseCase()
+    {
+        return new RegisterUserUseCase(null, null, null, null, null, null);
+    }
+}
