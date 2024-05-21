ffb570238dae9d5a04c32283baa1b3594f05d160
Welisson Arley <welissonarleyvs@gmail.com>
2024-05-21 22:16:16 +0100

    tests: finishing the test for success for register user use case

--- tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
+++ tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -1,5 +1,15 @@
-﻿namespace CommonTestUtilities.Repositories;
+﻿using CashFlow.Domain.Repositories.User;
+using Moq;
+
+namespace CommonTestUtilities.Repositories;
 public class UserReadOnlyRepositoryBuilder
 {
+    private readonly Mock<IUserReadOnlyRepository> _repository;
 
+    public UserReadOnlyRepositoryBuilder()
+    {
+        _repository = new Mock<IUserReadOnlyRepository>();
+    }
+
+    public IUserReadOnlyRepository Build() => _repository.Object;
 }
--- tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -29,7 +29,8 @@ public class RegisterUserUseCaseTest
         var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
         var passwordEncripter = PasswordEncripterBuilder.Build();
         var tokenGenerator = JwtTokenGeneratorBuilder.Build();
+        var readRepository = new UserReadOnlyRepositoryBuilder().Build();
 
-        return new RegisterUserUseCase(mapper, passwordEncripter, null, writeRepository, tokenGenerator, unitOfWork);
+        return new RegisterUserUseCase(mapper, passwordEncripter, readRepository, writeRepository, tokenGenerator, unitOfWork);
     }
 }
