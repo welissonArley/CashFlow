ffc6fd0fa42bf7af9823f30a1560bee7b42725c1
Welisson Arley <welissonarleyvs@gmail.com>
2024-05-28 21:05:53 +0100

    feat: adding new environment for integration tests

---
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace WebApi.Test;
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Test");
+    }
+}
--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,17 +1,16 @@
 ﻿using CommonTestUtilities.Requests;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 using System.Net.Http.Json;
 
 namespace WebApi.Test.Users.Register;
-public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 {
     private const string METHOD = "api/User";
 
     private readonly HttpClient _httpClient;
 
-    public RegisterUserTest(WebApplicationFactory<Program> webApplicationFactory)
+    public RegisterUserTest(CustomWebApplicationFactory webApplicationFactory)
     {
         _httpClient = webApplicationFactory.CreateClient();
     }
