e76ee19ebd957011db19c09c5714ad91734d8193
Welisson Arley <welissonarleyvs@gmail.com>
2024-05-30 20:02:53 +0100

    feat: adding logged user

---
+++ src/CashFlow.Domain/Services/LoggedUser/ILoggedUser.cs
@@ -0,0 +1,7 @@
+﻿using CashFlow.Domain.Entities;
+
+namespace CashFlow.Domain.Services.LoggedUser;
+public interface ILoggedUser
+{
+    Task<User> Get();
+}
---
+++ src/CashFlow.Infrastructure/Services/LoggedUser/LoggedUser.cs
@@ -0,0 +1,30 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Services.LoggedUser;
+using CashFlow.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace CashFlow.Infrastructure.Services.LoggedUser;
+internal class LoggedUser : ILoggedUser
+{
+    private readonly CashFlowDbContext _dbContext;
+
+    public LoggedUser(CashFlowDbContext dbContext) => _dbContext = dbContext;
+
+    public async Task<User> Get()
+    {
+        string token = "token";
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var jwtSecurityToken = tokenHandler.ReadJwtToken(token);
+
+        var identifier = jwtSecurityToken.Claims.First(claim => claim.Type == ClaimTypes.Sid).Value;
+
+        return await _dbContext
+            .Users
+            .AsNoTracking()
+            .FirstAsync(user => user.UserIdentifier == Guid.Parse(identifier));
+    }
+}
