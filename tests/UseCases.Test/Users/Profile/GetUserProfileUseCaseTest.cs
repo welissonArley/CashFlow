2871058270ffb3cd6a4a7bd83b6a278922b82460
Welisson Arley <welissonarleyvs@gmail.com>
2024-06-06 00:28:25 +0100

    tests: test to get user profile

---
+++ tests/UseCases.Test/Users/Profile/GetUserProfileUseCaseTest.cs
@@ -0,0 +1,31 @@
+﻿using CashFlow.Application.UseCases.Users.Profile;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using FluentAssertions;
+
+namespace UseCases.Test.Users.Profile;
+public class GetUserProfileUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+
+        var result = await useCase.Execute();
+
+        result.Should().NotBeNull();
+        result.Name.Should().Be(user.Name);
+        result.Email.Should().Be(user.Email);
+    }
+
+    private GetUserProfileUseCase CreateUseCase(User user)
+    {
+        var mapper = MapperBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new GetUserProfileUseCase(loggedUser, mapper);
+    }
+}
---
+++ tests/WebApi.Test/Users/Profile/GetUserProfileTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using System.Net;
+using System.Text.Json;
+
+namespace WebApi.Test.Users.Profile;
+public class GetUserProfileTest : CashFlowClassFixture
+{
+    private const string METHOD = "api/User";
+
+    private readonly string _token;
+    private readonly string _userName;
+    private readonly string _userEmail;
+
+    public GetUserProfileTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.User_Team_Member.GetToken();
+        _userName = webApplicationFactory.User_Team_Member.GetName();
+        _userEmail = webApplicationFactory.User_Team_Member.GetEmail();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var result = await DoGet(METHOD, _token);
+
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        response.RootElement.GetProperty("name").GetString().Should().Be(_userName);
+        response.RootElement.GetProperty("email").GetString().Should().Be(_userEmail);
+    }
+}
